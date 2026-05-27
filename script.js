be819c695e1c87d2a220567937f7bf1f17cc4a22
Alex Jones <joneale6@oregonstate.edu>
2026-05-26 20:37:38 -0700

    Revision

--- script.js
+++ script.js
@@ -27,19 +27,38 @@ addButton.addEventListener("click", function() {
         <h3>${workoutName}</h3>
         <p>Type: ${workoutType}</p>
         <p>Date: ${workoutDate}</p>
-        <p>Status : In Progress</p>
+        <p>Status: In Progress</p>
+        <p class="workout-details" style="display: none;">
+        Details: Use workout entries to track what type of workout you planned,
+         when you plan to do it, and whether or not it is completed.</p>
 
+        <button class="details-button">View Details</button>
         <button class="complete-button">Mark as Complete</button>
         <button class="delete-button">Delete</button>
     `;
 
     workoutList.appendChild(workoutEntry);
 
+    const detailsButton = workoutEntry.querySelector(".details-button");
+
+    const workoutDetails = workoutEntry.querySelector(".workout-details");
+
+    detailsButton.addEventListener("click", function() {
+        if (workoutDetails.style.display === "none") {
+            workoutDetails.style.display = "block";
+            detailsButton.textContent = "Hide Details";
+        } else {
+            workoutDetails.style.display = "none";
+            detailsButton.textContent = "View Details";
+        }
+
+    });
+
     const completeButton = workoutEntry.querySelector(".complete-button");
 
     completeButton.addEventListener("click", function() {
         const statusText = workoutEntry.querySelector("p:nth-of-type(3)");
-        statusText.textContent = "Status : Completed";
+        statusText.textContent = "Status: Completed";
     });
 
     const deleteButton = workoutEntry.querySelector(".delete-button");
